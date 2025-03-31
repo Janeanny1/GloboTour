74fe7b9bd365c2acd7baae50021d533227e86f54
Janeanny1 <janethmalikita@gmail.com>
2025-03-31 14:10:20 +0300

    Android assignment

--- app/src/main/java/com/example/globotour/MainActivity.kt
+++ app/src/main/java/com/example/globotour/MainActivity.kt
@@ -16,29 +16,27 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var toolbar        : MaterialToolbar
     private lateinit var navController  : NavController
-    private lateinit var bottomNavView : BottomNavigationView
+    private lateinit var bottomNavView  : BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main) 
 
-        // Initialize Views
+
         toolbar         = findViewById(R.id.activity_main_toolbar)
         bottomNavView = findViewById(R.id.bottom_nav_view)
 
 
-        // Get NavHostFragment and NavController
         val navHostFrag = supportFragmentManager.findFragmentById(R.id.nav_host_frag) as NavHostFragment
         navController   = navHostFrag.navController
 
-        // Define AppBarConfiguration: Connect DrawerLayout with Navigation Component
+
         val topLevelDestinations = setOf(R.id.fragmentCityList, R.id.fragmentFavoriteList)
         val appBarConfiguration = AppBarConfiguration(topLevelDestinations)
 
-        // Connect Toolbar with NavController
+
         toolbar.setupWithNavController(navController, appBarConfiguration)
 
-        // Connect NavigationView with NavController
         bottomNavView.setupWithNavController(navController)
     }
 
